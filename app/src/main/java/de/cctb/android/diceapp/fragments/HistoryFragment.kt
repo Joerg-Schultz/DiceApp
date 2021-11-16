a0180c97b81f96ca39fb81b8682764100394eed1
Joerg-Schultz <JoergSky@online.de>
2021-11-16 08:32:37 +0100

    History fragment skeleton

--- app/src/main/java/de/cctb/android/diceapp/fragments/HistoryFragment.kt
+++ app/src/main/java/de/cctb/android/diceapp/fragments/HistoryFragment.kt
@@ -1,6 +1,32 @@
 package de.cctb.android.diceapp.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import de.cctb.android.diceapp.databinding.HistoryFragmentBinding
+import de.cctb.android.diceapp.viewmodels.MainViewModel
 
 class HistoryFragment : Fragment() {
+
+    private var _binding: HistoryFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = HistoryFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
