780dd16ed159bc847d0fce33a1b97bc78b086502
Joerg-Schultz <JoergSky@online.de>
2021-11-16 08:07:04 +0100

    view binding

--- app/src/main/java/de/cctb/android/diceapp/fragments/MainFragment.kt
+++ app/src/main/java/de/cctb/android/diceapp/fragments/MainFragment.kt
@@ -7,10 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import de.cctb.android.diceapp.R
+import de.cctb.android.diceapp.databinding.MainFragmentBinding
 import de.cctb.android.diceapp.viewmodels.MainViewModel
 
 class MainFragment : Fragment() {
 
+    private var _binding: MainFragmentBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -21,13 +26,17 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
